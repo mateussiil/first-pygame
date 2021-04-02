92a66dcfacc0ef42b9f0c2b31fbce54e38f6fd32
Mateus Oliveira <mateussiiloliveira@gmal.com>
2021-04-02 10:39:08 -0300

    Criando uma classe para camera

---
+++ scripts/camera.py
@@ -0,0 +1,13 @@
+class Camera:
+    def __init__(self, game):
+        self.game = game
+        self.true_pos = [0, 0]
+        self.target_pos = [0,0]
+        
+    def set_target(self, pos):
+        self.target_pos = pos
+
+    def update(self):
+        self.true_pos[0] += (( self.target_pos[0] - self.true_pos[0]) - (800/3 + 10))/20
+        self.true_pos[1] += (( self.target_pos[1] - self.true_pos[1]) - (600/3 + 10))/20
+        return self.true_pos
