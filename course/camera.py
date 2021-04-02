1ed9e351f288311fe43c977a98183fbd6f8ec944
Mateus Oliveira <mateussiiloliveira@gmal.com>
2021-04-02 11:23:27 -0300

    :zap: Mudando collisao e move
    Collision e move devem ser de responsabilidade de entidade

--- course/camera.py
+++ course/camera.py
@@ -103,42 +103,6 @@ player_flip = False
 
 grass_sound_timer = 0
 
-
-def collision_test(rect, tiles):
-    hit_list = []
-    for tile in tiles:
-        if rect.colliderect(tile):
-            hit_list.append(tile)
-    return hit_list
-
-
-def move(rect, movement, tiles):
-    collision_types = {"top": False, "bottom": False,
-                       "right": False, "left": False}
-
-    rect.x += movement[0]
-    hit_list = collision_test(rect, tiles)
-    for tile in hit_list:
-        if movement[0] > 0:
-            rect.right = tile.left
-            collision_types["right"] = True
-        elif movement[0] < 0:
-            rect.left = tile.right
-            collision_types["left"] = True
-
-    rect.y += movement[1]
-    hit_list = collision_test(rect, tiles)
-    for tile in hit_list:
-        if movement[1] > 0:
-            rect.bottom = tile.top
-            collision_types["bottom"] = True
-        elif movement[1] < 0:
-            rect.top = tile.bottom
-            collision_types["top"] = True
-
-    return rect, collision_types
-
-
 moving_right = False
 moving_left = False
 moving_up = False
@@ -231,7 +195,8 @@ while True:
         player_action, player_frame = change_action(
             player_action, player_frame, 'run')
         player_flip = False
-    player_rect, collisions = move(player_rect, player_movement, tile_rects)
+    
+    player_rect, collisions = player.move(player_rect, player_movement, tile_rects)
 
     if collisions["bottom"]:
         player_y_momentum = 0
--- scripts/entity.py
+++ scripts/entity.py
@@ -4,5 +4,37 @@ class Entity:
         self.image = image
         self.width = image.get_width()
         self.height = image.get_height()
-        
     
+    def move(self, rect, movement, tiles):
+        collision_types = {"top": False, "bottom": False,
+                        "right": False, "left": False}
+
+        rect.x += movement[0]
+        hit_list = self.collision_test(rect, tiles)
+        for tile in hit_list:
+            if movement[0] > 0:
+                rect.right = tile.left
+                collision_types["right"] = True
+            elif movement[0] < 0:
+                rect.left = tile.right
+                collision_types["left"] = True
+
+        rect.y += movement[1]
+        hit_list = self.collision_test(rect, tiles)
+        for tile in hit_list:
+            if movement[1] > 0:
+                rect.bottom = tile.top
+                collision_types["bottom"] = True
+            elif movement[1] < 0:
+                rect.top = tile.bottom
+                collision_types["top"] = True
+
+        return rect, collision_types
+
+
+    def collision_test(self,rect, tiles):
+        hit_list = []
+        for tile in tiles:
+            if rect.colliderect(tile):
+                hit_list.append(tile)
+        return hit_list
